fae502fa083c9d89ff3c212fbe4d5cba01170e0f
ajithkumarazax <ajithkumarazax52@gmail.com>
2024-02-15 21:25:25 +0530

    New commit

---
+++ src/com/insta/Instagramlogin.java
@@ -0,0 +1,12 @@
+package com.insta;
+
+public class Instagramlogin {
+	private void tc1() {
+        System.out.println("Test1");
+        
+	}
+	private void tc2() {
+System.out.println("Test2");
+	}
+
+}
